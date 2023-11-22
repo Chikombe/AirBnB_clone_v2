8f824e7ce311fdf4420563918a4a29d59414bda5
Chikombe <chikombe.ecy@gmail.com>
2023-11-22 03:37:51 -0800

    updated console.py

--- console.py
+++ console.py
@@ -16,26 +16,22 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """The entry point for the command interpreter"""
 
-
     prompt = "(hbnb) "
-    __classes = {"BaseModel", "State", "User", "city",
+    __classes = {"BaseModel", "State", "User", "City",
                  "Place", "Amenity", "Review"}
 
     def emptyline(self):
         """Ignore empty spaces."""
         pass
 
-
     def do_quit(self, line):
         """Quit command to exit the program."""
         return True
-
 
     def do_EOF(self, line):
         """EOF signal to exit the program."""
         print("")
         return True
-
 
     def do_create(self, line):
         """ Creates an object with given parameters
@@ -45,7 +41,6 @@ class HBNBCommand(cmd.Cmd):
             if not line:
                 raise SyntaxError()
             my_list = line.split(" ")
-
 
             kwargs = {}
             for i in range(1, len(my_list)):
@@ -59,7 +54,6 @@ class HBNBCommand(cmd.Cmd):
                         continue
                 kwargs[key] = value
 
-
             if kwargs == {}:
                 obj = eval(my_list[0])()
             else:
@@ -71,8 +65,7 @@ class HBNBCommand(cmd.Cmd):
         except SyntaxError:
             print("** class name missing **")
         except NameError:
-            print("** class doesn't exist**")
-
+            print("** class doesn't exist **")
 
     def do_show(self, line):
         """Prints the string representation of an instance
@@ -104,7 +97,6 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         except KeyError:
             print("** no instance found **")
-
 
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id
@@ -138,7 +130,6 @@ class HBNBCommand(cmd.Cmd):
         except KeyError:
             print("** no instance found **")
 
-
     def do_all(self, line):
         """Usage: all or all <class> or <class>.all()
         Display string representations of all instances of a given class.
@@ -148,7 +139,7 @@ class HBNBCommand(cmd.Cmd):
             print([o[k].__str__() for k in o])
             return
         try:
-            args = args.split(" ")
+            line = line.split(" ")
             if args[0] not in self.__classes:
                 raise NameError()
 
@@ -159,18 +150,8 @@ class HBNBCommand(cmd.Cmd):
         except NameError:
             print("** class doesn't exist **")
 
-
-    def do_count(self, line):
-        """Count current number of class instances"""
-        count = 0
-        for k, v in storage._FileStorage__objects.items():
-            if args == k.split('.')[0]:
-                count += 1
-        print(count)
-
-
     def do_update(self, line):
-        """Updates an instanceby adding or updating attribute
+        """Updates an instance by adding or updating attribute
         Exceptions:
             SyntaxError: when there is no args given
             NameError: when there is no object taht has the name
@@ -213,7 +194,72 @@ class HBNBCommand(cmd.Cmd):
             print("** attribute name missing **")
         except ValueError:
             print("** value missing **")
-    
 
-if __name__ == "__main__":
+    def count(self, line):
+        """count the number of instances of a class
+        """
+        counter = 0
+        try:
+            my_list = split(line, " ")
+            if my_list[0] not in self.__classes:
+                raise NameError()
+            objects = storage.all()
+            for key in objects:
+                name = key.split('.')
+                if name[0] == my_list[0]:
+                    counter += 1
+            print(counter)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def strip_clean(self, args):
+        """strips the argument and return a string of command
+        Args:
+            args: input list of args
+        Return:
+            returns string of argumetns
+        """
+        isi_list = []
+        isi_list.append(args[0])
+        try:
+            my_dict = eval(
+                args[1][args[1].find('{'):args[1].find('}')+1])
+        except Exception:
+            my_dict = None
+        if isinstance(my_dict, dict):
+            new_str = args[1][args[1].find('(')+1:args[1].find(')')]
+            isi_list.append(((new_str.split(", "))[0]).strip('"'))
+            isi_list.append(my_dict)
+            return isi_list
+        new_str = args[1][args[1].find('(')+1:args[1].find(')')]
+        isi_list.append(" ".join(new_str.split(", ")))
+        return " ".join(i for i in isi_list)
+
+    def default(self, line):
+        """retrieve all instances of a class and
+        retrieve the number of instances
+        """
+        my_list = line.split('.')
+        if len(my_list) >= 2:
+            if my_list[1] == "all()":
+                self.do_all(my_list[0])
+            elif my_list[1] == "count()":
+                self.count(my_list[0])
+            elif my_list[1][:4] == "show":
+                self.do_show(self.strip_clean(my_list))
+            elif my_list[1][:7] == "destroy":
+                self.do_destroy(self.strip_clean(my_list))
+            elif my_list[1][:6] == "update":
+                args = self.strip_clean(my_list)
+                if isinstance(args, list):
+                    obj = storage.all()
+                    key = args[0] + ' ' + args[1]
+                    for k, v in args[2].items():
+                        self.do_update(key + ' "{}" "{}"'.format(k, v))
+                else:
+                    self.do_update(args)
+        else:
+            cmd.Cmd.default(self, line)
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
